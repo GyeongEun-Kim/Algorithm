feeed9a12c452bc7fe6da7f755a43118561775d9
김경은 <73164845+GyeongEun-Kim@users.noreply.github.com>
2023-12-13 20:32:45 +0900

    [Silver III] Title: 통계학, Time: 720 ms, Memory: 65784 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -8,37 +8,37 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        Map<String, Integer> count = new HashMap<>();
+        Map<Integer, Integer> count = new HashMap<>(); //key를 string으로 하는이유???
         int[] arr = new int[n];
-        int sum=0;
+        int sum = 0;
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
-        for (int i=0;i<n;i++) {
+        for (int i = 0; i < n; i++) {
             int num = Integer.parseInt(br.readLine());
-            sum+=num;
+            sum += num;
             arr[i] = num;
             max = Math.max(max, num);
-            min = Math.min(min,num);
-            count.put(String.valueOf(num),count.getOrDefault(String.valueOf(num),0)+1);
+            min = Math.min(min, num);
+            count.put(num, count.getOrDefault(num, 0) + 1);
         }
 
 
         //산술평균
-        double divide = (double)sum/n;
-        if (divide<0) System.out.println(Math.round(divide));
+        double divide = (double) sum / n;
+        if (divide < 0) System.out.println(Math.round(divide) );
         else System.out.println(Math.round(divide));
 
         //중앙값
         Arrays.sort(arr);
-        System.out.println(arr[n/2]);
+        System.out.println(arr[n / 2]);
 
         //최빈값
-        List<String> keySet = new ArrayList<>(count.keySet());
+        List<Integer> keySet = new ArrayList<>(count.keySet());
         keySet.sort(((o1, o2) -> {
             if (count.get(o2) - count.get(o1) > 0)
-                return 1;
-            else if (count.get(o2) - count.get(o1) == 0)
-                return Integer.parseInt(o1)- Integer.parseInt(o2);
+                return 1; //빈도수 내림차순 정렬
+            else if (count.get(o2) - count.get(o1) == 0) //빈도수가 같다면?
+                return o1-o2; //숫자는 오름차순 정렬
             else return -1;
         }));
 //
@@ -48,13 +48,12 @@ public class Main {
 //        }
 
 
-        if (count.size()>1 && count.get(keySet.get(0)) == count.get(keySet.get(1))) {
+        if (count.size() > 1 && count.get(keySet.get(0)) == count.get(keySet.get(1))) {
             System.out.println(keySet.get(1));
-        }
-        else System.out.println(keySet.get(0));
+        } else System.out.println(keySet.get(0));
 
         //범위
-        System.out.println(max-min);
+        System.out.println(max - min);
 
     }
 }
